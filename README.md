b1993ae03e943b99dc49ba976fd44c5d5d85b457
Tobias Augspurger <ly0@protonmail.com>
2024-05-12 20:22:04 +0200

    add first prototypes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# osca
-Open Source in Climate and Sustainability - A collaborative workspace to quantify the impact of the open source movement on climate change and sustainability. 
+# Open Sustainable Technology Analytics 
+A collaborative workspace to quantify the impact of the open source movement on climate change and sustainability. 
+
+
---
+++ opensustainTemplate.py
@@ -0,0 +1,37 @@
+import plotly.io as pio
+import plotly.graph_objects as go
+import plotly.express as px
+from PIL import Image
+
+logo_img = Image.open("logo.png")  # image path
+# default plotting options
+# Palette https://coolors.co/palette/0e7c7b-17bebb-ffc857-e9724c-c5283d
+marker_color = "#0E7C7B"
+color_continuous_scale = [[0, "#3b52ff"], [1, "#169485"]]
+color_discrete_sequence = ["#3361e4","#2b6fc9","#227eae","#1a8d93","#169485"]
+boarder_color = "#040404"
+color_divergent_scale = [[0, "#3b52ff"], [0.5, "#F6157"], [1, "#169485"]]
+
+# OpenSustain.Tech colours
+# marker_color = "#159485"
+# boarder_color = "#000000"
+# color_continuous_scale = px.colors.make_colorscale(["#26E5A9","#3E59FB"])
+color_divergent_scale = px.colors.make_colorscale(["#3b52ff", "#F6157","#169485"])
+# color_discrete_sequence = ["#3e59fb","#007dff","#0097ff","#00acff","#00bef9","#00cdde","#00dac1","#26e5a9"]
+
+# Register your theme as a named template
+pio.templates["OpenSustain"] = go.layout.Template(
+    layout=dict(
+        margin=dict(b=0),  # bottom margin
+        font=dict(
+            family="Open Sans",
+            color="#040404",
+            size=15,
+        ),
+        title_font_family="Open Sans",
+        title_font_color="#040404",
+    ),
+)
+
+# Combine your theme with plotly's default
+pio.templates.default = "plotly+OpenSustain"
